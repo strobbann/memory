c360788a49f6d0fb17f0d715e51eb7c6b2809bd0
Robert Savela <strobbann@gmail.com>
2017-09-13 12:16:01 +0200

    Added Type To Card Models

--- game/src/js/memory.model.Card.js
+++ game/src/js/memory.model.Card.js
@@ -1,10 +1,22 @@
 memory.model.Card = class {
-  constructor(id, frontImg, backImg) {
+  constructor(id, frontImg, backImg, type) {
     this.id = id;
     this.frontImg = frontImg;
     this.backImg = backImg;
+    this.type = type;
   }
-  isSameCard(id) {
-    return this.id === id;
+  isSameCard(type) {
+    return this.type === type;
   }
+};
+memory.model.Card.Type = {
+  BEAR: 'Bear',
+  BUNNY: 'Bunny',
+  CHICKEN: 'Chicken',
+  DOG: 'Dog',
+  DUCK: 'Duck',
+  ELEPHANT: 'Elephant',
+  PANDA: 'Panda',
+  PIG: 'Pig',
+  TIGER: 'Tiger',
 };

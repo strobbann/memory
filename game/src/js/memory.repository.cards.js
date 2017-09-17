3f1581e49f10c96b33f3dbaa3e10827932d2b927
Robert Savela <strobbann@gmail.com>
2017-09-17 18:41:15 +0200

    Refactor (#12)
    * added notClickable on same card and clearBoard function
    
    * minor refactoring of model and repos and game

--- game/src/js/memory.repository.cards.js
+++ game/src/js/memory.repository.cards.js
@@ -1,11 +1,12 @@
-memory.repository.cards = [];
-memory.repository.cards.push(new memory.model.Card(1, 'img/bear.png', memory.model.Card.Type.BEAR));
-memory.repository.cards.push(new memory.model.Card(2, 'img/bear.png', memory.model.Card.Type.BEAR));
-memory.repository.cards.push(new memory.model.Card(3, 'img/bunny.png', memory.model.Card.Type.BUNNY));
-memory.repository.cards.push(new memory.model.Card(4, 'img/bunny.png', memory.model.Card.Type.BUNNY));
-memory.repository.cards.push(new memory.model.Card(5, 'img/chicken.png', memory.model.Card.Type.CHICKEN));
-memory.repository.cards.push(new memory.model.Card(6, 'img/chicken.png', memory.model.Card.Type.CHICKEN));
-memory.repository.cards.push(new memory.model.Card(7, 'img/duck.png', memory.model.Card.Type.DUCK));
-memory.repository.cards.push(new memory.model.Card(8, 'img/duck.png', memory.model.Card.Type.DUCK));
-memory.repository.cards.push(new memory.model.Card(9, 'img/dog.png', memory.model.Card.Type.DOG));
-memory.repository.cards.push(new memory.model.Card(10, 'img/dog.png', memory.model.Card.Type.DOG));
+memory.repository.cards = [
+  new memory.model.Card(1, 'img/bear.png', memory.model.Card.Type.BEAR),
+  new memory.model.Card(2, 'img/bear.png', memory.model.Card.Type.BEAR),
+  new memory.model.Card(3, 'img/bunny.png', memory.model.Card.Type.BUNNY),
+  new memory.model.Card(4, 'img/bunny.png', memory.model.Card.Type.BUNNY),
+  new memory.model.Card(5, 'img/chicken.png', memory.model.Card.Type.CHICKEN),
+  new memory.model.Card(6, 'img/chicken.png', memory.model.Card.Type.CHICKEN),
+  new memory.model.Card(7, 'img/duck.png', memory.model.Card.Type.DUCK),
+  new memory.model.Card(8, 'img/duck.png', memory.model.Card.Type.DUCK),
+  new memory.model.Card(9, 'img/dog.png', memory.model.Card.Type.DOG),
+  new memory.model.Card(10, 'img/dog.png', memory.model.Card.Type.DOG),
+];
